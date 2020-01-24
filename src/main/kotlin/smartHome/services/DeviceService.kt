60f127b3b135bffc094fdea6d3a861395c841e4a
Albin <albin.92@live.se>
2020-01-24 20:49:57 +0100

    cleaning

--- src/main/kotlin/smartHome/services/DeviceService.kt
+++ src/main/kotlin/smartHome/services/DeviceService.kt
@@ -2,23 +2,23 @@ package smartHome.services
 
 import java.util.*
 
-val protocol = 4
-val codes_on = arrayOf("658800", "587188", "587196", "448418")
-val codes_off = arrayOf("151184", "99108", "448428", "587186")
+const val PROTOCOL = 4
+val CODES_ON = arrayOf("658800", "587188", "587196", "448418")
+val CODES_OFF = arrayOf("151184", "99108", "448428", "587186")
 
 
 fun deviceOn(id : Int) {
   println("Device $id on")
-  sendCode(codes_on[id])
+  sendCode(CODES_ON[id])
 }
 
 fun deviceOff(id : Int) {
   println("Device $id off")
-  sendCode(codes_off[id])
+  sendCode(CODES_OFF[id])
 }
 
 fun sendCode(code: String) {
-  val proc = Runtime.getRuntime().exec("src/main/resources/codesend $code $protocol")
+  val proc = Runtime.getRuntime().exec("src/main/resources/codesend $code $PROTOCOL")
   Scanner(proc.inputStream).use {
     while (it.hasNextLine()) println(it.nextLine())
   }
